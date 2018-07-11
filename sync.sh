e8c1a60603e14ebb527bbc4cbf82d5a9fea37b72
Chris Schuld <chris@chrisschuld.com>
2018-07-11 12:16:40 -0700

    updates, complete

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-s3cmd sync -P /var/www/api.inspectionsupport.net/* s3://api.inspectionsupport.net
+s3cmd sync -P /var/www/api.inspectionsupport.net/public/ s3://api.inspectionsupport.net
